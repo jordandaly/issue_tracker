0290cfe6f48436c546514d9a2c225d78a2b9bf70
Jordan Daly <jordandaly@gmail.com>
2018-07-23 16:57:43 +1200

    fixed image path issue

--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -125,4 +125,4 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
