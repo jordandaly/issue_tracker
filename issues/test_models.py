a4ba882ad4437ae9b51ea23f44693cf2aeebee5b
jordan <jordandaly@gmail.com>
2018-08-10 15:43:52 +1200

    updated test_models and test_views

--- issues/test_models.py
+++ issues/test_models.py
@@ -5,20 +5,33 @@ from django.contrib.auth.models import User
 
 class TestIssueModel(TestCase):
 
-    def test_defaults_issue_type_bug_status_todo_upvotes_0_isResolved_False(self):
-        user = User()
-        user.save()
-        issue = Issue(author=user, price=100)
-        issue.save()
-        self.assertEqual(issue.issue_type, "bug")
-        self.assertEqual(issue.status, "todo")
-        self.assertEqual(issue.upvotes, 0)
-        self.assertFalse(issue.is_resolved)
 
-    def test_can_create_an_issue_with_a_title_and_description(self):
+    def test_create_issue(self):
         user = User()
         user.save()
-        issue = Issue(title="Test Issue Title", description="Test Issue Description",author=user, price=100)
+        issue = Issue(title="Test Issue Title", description="Test Issue Description", author=user, price=200, tag="TEST")
         issue.save()
         self.assertEqual(issue.title, "Test Issue Title")
         self.assertEqual(issue.description, "Test Issue Description")
+        self.assertEqual(issue.author, user)
+        self.assertIsNone(issue.assignee)
+        self.assertIsNone(issue.resolved_date)
+        self.assertEqual(issue.price, 200)
+        self.assertEqual(issue.issue_type, "bug")
+        self.assertEqual(issue.status, "todo")
+        self.assertEqual(issue.tag, "TEST")
+        self.assertEqual(issue.upvotes, 0)
+        self.assertFalse(issue.is_resolved)
+
+    def test_create_comment(self):
+        user = User()
+        user.save()
+        issue = Issue(title="Test Issue Title", description="Test Issue Description", author=user, price=200)
+        issue.save()
+        comment = Comment(comment="Test Issue Comment", author=user, issue=issue)
+        comment.save()
+        self.assertEqual(comment.comment, "Test Issue Comment")
+        self.assertEqual(comment.author, user)
+        self.assertEqual(comment.issue, issue)
+
+
--- issues/test_views.py
+++ issues/test_views.py
@@ -16,6 +16,11 @@ class TestViews(LoggedInTestCase):
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, "issues.html")
 
+    def test_get_search_issues_page(self):
+        page = self.client.get("/issues/search/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "search_issues.html")
+
 
     def test_get_add_issue_page(self):
         page = self.client.get("/issues/new/")
@@ -49,3 +54,31 @@ class TestViews(LoggedInTestCase):
     def test_get_issue_detail_page_for_item_that_does_not_exist(self):
         page = self.client.get("/issues/1/")
         self.assertEqual(page.status_code, 404)
+
+
+    def test_get_add_comment_page(self):
+        user = User()
+        user.save()
+        issue = Issue(title="Test Issue Title", description="Test Issue Description", author=user, price=100)
+        issue.save()
+        page = self.client.get("/issues/{0}/new/".format(issue.id))
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "commentform.html")
+
+    def test_get_edit_comment_page(self):
+        user = User()
+        user.save()
+        issue = Issue(title="Test Issue Title", description="Test Issue Description", author=user, price=100)
+        issue.save()
+        comment = Comment(comment="Test Issue Comment", author=user, issue=issue)
+        comment.save()
+
+        page = self.client.get("/issues/{0}/{1}/edit/".format(issue.id, comment.id))
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "commentform.html")
+
+
+    def test_get_edit_comment_page_for_item_that_does_not_exist(self):
+        page = self.client.get("/issues/1/1/edit/")
+        self.assertEqual(page.status_code, 404)
+
