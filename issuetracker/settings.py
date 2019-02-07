41ed746e0f1b5d59d5d7e947dc4988974f2638c4
Jordan Daly <jordandaly@gmail.com>
2019-02-07 22:50:14 +0000

    set debug to false and updated README

--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','daly-issue-tracker.herokuapp.com']
 
